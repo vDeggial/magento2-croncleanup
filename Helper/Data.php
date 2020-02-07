32c0daa9b75fb9cb67dcbdebf80a630ee6074ee6
Oleg K <voknevark@gmail.com>
2020-02-06 18:18:45 -0800

    ObjectManager Inject

--- Helper/Data.php
+++ Helper/Data.php
@@ -1,29 +1,35 @@
 <?php
-
 namespace Hapex\CronCleanup\Helper;
 
 use Hapex\Core\Helper\DataHelper;
+use Magento\Framework\ObjectManagerInterface;
 use Magento\Framework\App\Helper\Context;
 
 class Data extends DataHelper
 {
-    public function isEnabled()
-    {
-        return $this->getConfigFlag('hapex_croncleanup/general/enable');
-    }
-    
-    public function getInterval()
-    {
-        return $this->getConfigValue('hapex_croncleanup/general/interval');
-    }
-    
-    public function getIntervalRunning()
-    {
-        return $this->getConfigValue('hapex_croncleanup/general/interval_running');
-    }
-    
-    public function log($message)
-    {
-        $this->printLog("hapex_cron_cleanup", $message);
-    }
+	public function __construct(Context $context, ObjectManagerInterface $objectManager)
+	{
+
+		parent::__construct($context, $objectManager);
+	}
+
+	public function isEnabled()
+	{
+		return $this->getConfigFlag('hapex_croncleanup/general/enable');
+	}
+
+	public function getInterval()
+	{
+		return $this->getConfigValue('hapex_croncleanup/general/interval');
+	}
+
+	public function getIntervalRunning()
+	{
+		return $this->getConfigValue('hapex_croncleanup/general/interval_running');
+	}
+
+	public function log($message)
+	{
+		$this->printLog("hapex_cron_cleanup", $message);
+	}
 }
